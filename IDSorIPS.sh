5b5f8b509acb60a89dbc2edce7bf8b4170660844
AbdulRhman Alfaifi <a.alfaifi.14@gmail.com>
2018-05-13 23:39:07 +0300

    Bug Fix

--- IDSorIPS.sh
+++ IDSorIPS.sh
@@ -9,15 +9,15 @@ echo -e "[${RED}2${RE}] Intrusion Prevention System (IPS)"
 read -p "Run on which mode : " choose
 if [[ $choose = 1 ]]
 then
-  cp IDSorIPS/IDS/* html/inc/
-  rm html/inc/snortConfig.php 2> /dev/null
+  cp IDSorIPS/IDS/* /var/www/html/inc/
+  rm /var/www/html/inc/snortConfig.php 2> /dev/null
   echo -e "${GREEN}Snort is Runing as IDS${RE}";
 elif [[ $choose = 2 ]]
 then
   echo -e -n "Enter two interfaces ${GREEN}(${RE}interface1:interface2${GREEN})${RE} : "
   read interfaces
   echo '<?php $interface = "'$interfaces'";?>' > IDSorIPS/IPS/snortConfig.php
-  cp IDSorIPS/IPS/* html/inc/
+  cp IDSorIPS/IPS/* /var/www/html/inc/
   echo -e "${GREEN}Snort is Runing as IPS${RE}";
 else
   echo -e "${RED}WRONG OPTION !${RE}"
